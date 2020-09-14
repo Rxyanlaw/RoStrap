397e6a2fb56dad5f7d678e5200ab410b21f3f421
480426051320090220 <a4804260513@gmail.com>
2020-09-13 21:22:30 -0400

    Update Settings.lua

--- Settings.lua
+++ Settings.lua
@@ -1,10 +1,9 @@
-{
-  
-  
-  
-  
-  
-  
-  
-  
-  M.ActivationKey = ("")
+local ReplicatedStorage
+game.plugins.RoStrap:GetService("ReplicatedStorage")
+instance.new.("ScreenGui")
+game.StarterGui.ScreenGui.Name = ("RoStrap")
+local RoStrap = game.StarterGui.RoStrap.TextScaled = true
+if game.StarterGui.RoStrap.TextScaled == true then
+  local Players = game.Players.LocalPlayer:GetService("Players"):Kick("Parent Change Detected")
+  end
+    
